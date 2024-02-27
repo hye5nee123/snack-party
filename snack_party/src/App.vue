d3b0e8e6288feab780c4483450fb3c738e6eb924
codenamehj <loveim1212@naver.com>
2024-02-27 22:55:26 +0900

    20240227 이효진 폰트어썸 부트 스트랩 설치/적용 css primary 색상 노랑색으로 변경

--- snack_party/src/App.vue
+++ snack_party/src/App.vue
@@ -1,30 +1,32 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div>
+  <Header />
+  </div>
+  <div>
+    <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </nav>
+  </div>
+  <router-view />
+  <div>
+    <Footer />
+  </div>
 </template>
 
+<script>
+import Header from './layout/Header.vue'
+import Footer from './layout/Footer.vue'
+
+export default {
+  name: "App",
+  components: {
+    Header,
+    Footer
+  }
+};
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
+@import "./assets/css/style.css";
 </style>
