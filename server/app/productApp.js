a8dc0da46a4a0ecf29e25daa1323897882c95742
admin <admin@DESKTOP-9K6N0AO>
2024-02-28 12:24:27 +0900

    20240228 이효진 commit

--- server/app/productApp.js
+++ server/app/productApp.js
@@ -1,1 +1,11 @@
-// 아자아자 파이팅!!
+const express = require('express');
+const productRouter = express.Router();
+const db = require('../db.js');
+
+// 전체조회
+app.get('/product', async(req, res)=>{
+    let list = await db.connection('productList',data);
+    res.send(list);
+});
+
+module.exports = productRouter;
