f3dc6bc6e7c1fb081aaac920845503288e88df14
혜원 <152113818+hey5nee123@users.noreply.github.com>
2024-02-28 12:27:47 +0900

    20240228 조혜원 notice commit 확인용

---
+++ server/db/sql/reviewSql.js
@@ -0,0 +1,13 @@
+const reviewList = `SELECT
+        review_code,
+        detail_code,
+        review_title,
+        review_content,
+        stars,
+        review_date,
+        member_code
+         `;
+
+module.exports = {
+  reviewList,
+};
