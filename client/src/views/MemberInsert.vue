3d0beaae5daffe79b2fdf5d766d701336ce83620
ONE_mac <shinhw91@naver.com>
2024-03-02 21:00:54 +0900

    20240302 신혜원 회원가입 기능 작업중

--- client/src/views/MemberInsert.vue
+++ client/src/views/MemberInsert.vue
@@ -20,43 +20,42 @@
                       <div class="col-md-12 col-lg-6">
                           <div class="form-item w-100">
                               <label class="bold">아이디<sup>*</sup></label>
-                              <input type="text" class="form-control">
+                              <input type="text" class="form-control" id="id" v-model="memberInfo.id">
                               <p class="form-label">5~20자, 영문 소문자, 숫자만 사용 가능</p>
                           </div>
                       </div>
                       <div class="col-md-12 col-lg-6 my-3">
                           <div class="form-item w-100 my-3">
-                              <!-- <label class="form-label my-3">Last Name<sup>*</sup></label> -->
                               <button type="button" class="btn border-secondary px-4 text-primary">중복확인</button>
                           </div>
                       </div>
                   </div>
                   <div class="form-item col-lg-6">
                       <label class="bold">비밀번호<sup>*</sup></label>
-                      <input type="password" class="form-control">
+                      <input type="password" class="form-control" id="pw" v-model="memberInfo.pw">
                       <p class="form-label">8~16자, 영문 대/소문자, 숫자, 특수문자(~!@#$%^&*)만 사용 가능</p>
                   </div>
                   <div class="form-item col-lg-6 my-3">
                       <label class="bold">비밀번호 확인<sup>*</sup></label>
-                      <input type="password" class="form-control">
+                      <input type="password" class="form-control" id="pw_confirm">
                   </div>
                   <div class="form-item col-lg-6 my-3">
                       <label class="bold">이름<sup>*</sup></label>
-                      <input type="text" class="form-control">
+                      <input type="text" class="form-control" id="name" v-model="memberInfo.name">
                   </div>
                   <div class="form-item col-lg-6 my-3">
                       <label class="bold">휴대전화<sup>*</sup></label>
-                      <input type="text" class="form-control" placeholder="'-'없이 숫자만 입력">
+                      <input type="text" class="form-control" placeholder="'-'없이 숫자만 입력" id="phone" v-model="memberInfo.phone">
                   </div>
                   <div class="form-item col-lg-6 my-3">
                       <label class="bold">이메일<sup>*</sup></label>
-                      <input type="text" class="form-control">
+                      <input type="text" class="form-control" id="email" v-model="memberInfo.email">
                   </div>
                   <div class="row">
                       <div class="col-md-12 col-lg-6">
                           <div class="form-item w-100">
                               <label class="bold">생년월일<sup></sup></label>
-                              <input type="text" class="form-control" placeholder="생년월일 8자리">
+                              <input type="text" class="form-control" placeholder="생년월일 8자리" id="birth" v-model="memberInfo.birth">
                           </div>
                       </div>
                       <div class="col-md-12 col-lg-6">
@@ -73,7 +72,7 @@
                       <div class="col-md-12 col-lg-6 my-3">
                           <div class="form-item w-100">
                               <label class="bold">기본주소<sup>*</sup></label>
-                              <input type="text" class="form-control">
+                              <input type="text" class="form-control" id="postcode" v-model="memberInfo.postcode">
                           </div>
                       </div>
                       <div class="col-md-12 col-lg-6">
@@ -83,56 +82,13 @@
                           </div>
                       </div>
                   </div>
-
-
-
-
-
-
-
-
-
-
-
-
                   <div class="form-item">
-                      <label class="form-label my-3">주소<sup>*</sup></label>
-                      <input type="text" class="form-control" placeholder="House Number Street Name">
-                  </div>
-                  <div class="form-item">
-                      <label class="form-label my-3">Town/City<sup>*</sup></label>
-                      <input type="text" class="form-control">
-                  </div>
-                  <div class="form-item">
-                      <label class="form-label my-3">Country<sup>*</sup></label>
-                      <input type="text" class="form-control">
-                  </div>
-                  <div class="form-item">
-                      <label class="form-label my-3">Postcode/Zip<sup>*</sup></label>
-                      <input type="text" class="form-control">
-                  </div>
-                  <div class="form-item">
-                      <label class="form-label my-3">Mobile<sup>*</sup></label>
-                      <input type="tel" class="form-control">
-                  </div>
-                  <div class="form-item">
-                      <label class="form-label my-3">Email Address<sup>*</sup></label>
-                      <input type="email" class="form-control">
-                  </div>
-                  <div class="form-check my-3">
-                      <input type="checkbox" class="form-check-input" id="Account-1" name="Accounts" value="Accounts">
-                      <label class="form-check-label" for="Account-1">Create an account?</label>
-                  </div>
-                  <hr>
-                  <div class="form-check my-3">
-                      <input class="form-check-input" type="checkbox" id="Address-1" name="Address" value="Address">
-                      <label class="form-check-label" for="Address-1">Ship to a different address?</label>
-                  </div>
-                  <div class="form-item">
-                      <textarea name="text" class="form-control" spellcheck="false" cols="30" rows="11" placeholder="Oreder Notes (Optional)"></textarea>
+                      <!-- <label class="form-label my-3">주소<sup>*</sup></label> -->
+                      <input type="text" class="form-control" id="address" v-model="memberInfo.address">
+                      <input type="text" class="form-control my-3" id="address_detail" v-model="memberInfo.address_detail">
                   </div>
                   <div class="row g-4 text-center align-items-center justify-content-center pt-4">
-                      <button type="button" class="btn border-secondary py-3 px-4 text-uppercase w-100 text-primary">가입신청</button>
+                      <button type="button" class="btn border-secondary py-3 px-4 text-uppercase w-100 text-primary" @click="memberInsert()">가입신청</button>
                   </div>
               </div>
               <!-- <div class="col-md-12 col-lg-6 col-xl-5">
@@ -278,7 +234,50 @@
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
-    name: 'LoginForm'
+    name: 'MemberInsert',
+    data() {
+        return {
+            memberInfo : {
+                id : '',
+                pw : '',
+                name : '',
+                phone : '',
+                email : '',
+                birth : '',
+                gender : '',
+                postcode : '',
+                address : '',
+                address_detail : ''
+            }
+        };
+    },
+    methods : {
+        async memberInsert() {
+            let data = {
+                param : {
+                    member_id : this.memberInfo.id,
+                    pw : this.memberInfo.pw,
+                    member_name : this.memberInfo.name,
+                    member_phone : this.memberInfo.phone,
+                    member_email : this.memberInfo.email,
+                    birthday : this.memberInfo.birth,
+                    gender : this.memberInfo.gender,
+                    postcode : this.memberInfo.postcode,
+                    member_type : 'b01',
+                    address : this.memberInfo.address,
+                    address_detail : this.memberInfo.address_detail
+                }
+            };
+            let result = await axios.post("/api/member", data)
+                        .catch(err => console.log(err));
+            let info = result.data.insertId;
+            if(info > 0) {
+                alert('회원가입이 완료되었습니다.');
+            }
+        }
+    }
 }
 </script>
