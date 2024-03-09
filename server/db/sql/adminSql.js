d3ecb15174c097322f8bcf99501d2748cf584364
kimhj365 <khj6428@gmail.com>
2024-03-09 18:04:40 +0900

    20240309 김현준 관리자 주문관리

--- server/db/sql/adminSql.js
+++ server/db/sql/adminSql.js
@@ -18,8 +18,31 @@ const inquiryNotAnswered =
 FROM   inquiry
 WHERE  answer_status is null`;
 
+//나의 전체 주문 목록 (-외 몇 개) + 페이징
+const orderListPage =
+`SELECT o.order_code
+      , member_code
+      , DATE_FORMAT(order_date, '%Y-%m-%d') as order_date
+      , merchant_uid
+      , total_price
+      , order_status
+      , DATE_FORMAT(cancel_date, '%Y-%m-%d') as cancel_date
+      , imp_uid
+      , p.product_name
+      , COUNT(d.order_code)-1 as buy_cnt
+FROM orders o JOIN detail d
+				        ON o.order_code = d.order_code
+              JOIN product p
+				        ON d.product_code = p.product_code `
+
+const orderListCount = 
+`SELECT COUNT(*) count
+FROM orders`
+
 module.exports = {
     salesDaily,
     salesMonthly,
-    inquiryNotAnswered
+    inquiryNotAnswered,
+    orderListPage,
+    orderListCount,
 }
