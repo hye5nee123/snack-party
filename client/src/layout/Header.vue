68328a19866fb4dfca0f5d7300bbc4ff2534d6b4
kimhj365 <khj6428@gmail.com>
2024-03-12 09:01:08 +0900

    20240312 김현준 메인, 헤더 수정

--- client/src/layout/Header.vue
+++ client/src/layout/Header.vue
@@ -4,10 +4,7 @@
         <div class="container topbar bg-primary d-none d-lg-block">
             <div class="d-flex justify-content-between">
                 <div class="top-info ps-2">
-                    <small class="me-3"><i class="fas fa-map-marker-alt me-2 text-secondary"></i> <a href="#"
-                            class="text-white">123 Street, New York</a></small>
-                    <small class="me-3"><i class="fas fa-envelope me-2 text-secondary"></i><a href="#"
-                            class="text-white">Email@Example.com</a></small>
+                    
                 </div>
                 <div class="top-link pe-2">
 
